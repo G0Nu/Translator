d76affd50d2743cc8d225aca4c0804fb81ddce77
Diego Nuno <diegonuo@outlook.com>
2024-10-11 20:48:34 -0600

    cleaning the data

---
+++ datacleaning.py
@@ -0,0 +1,8 @@
+string = "Hello how are you?, I'm fine thank you. I'm very glad you are here. Because you are a a nice person. Yahoo"
+def Cleaning(x):
+    newstring = x.split(".")
+    print(newstring)
+print(string,"\n")
+
+Cleaning(string)
+
