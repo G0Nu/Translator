bd271ba7523a5d77ec0a7cbbbd25addab337ac2d
Diego Nuno <diegonuo@outlook.com>
2024-10-25 02:18:52 -0600

    Data cleaning v2 tests // bug detected when inserting 2 following signs like: .., .?, ?., etc. || Also is require to clean code

--- datacleaning.py
+++ datacleaning.py
@@ -1,4 +1,6 @@
-
+from hmac import new
+from re import X
+from tkinter import Y
 from isort import file
 import nltk 
 from nltk.corpus import stopwords
@@ -14,25 +16,56 @@ nltk.download('wordnet')
 f = open('ch_books.text','r')
 content = f.read()
 f.close()
+all_words = []
 
 
-'''
-WITHOUT NLTK:
-string = "Hello how are you?, I'm fine thank you. I'm very glad you are here. Because you are a nice person. Yahoo"
+#WITHOUT NLTK:
+string = "Hello how are you?, I'm fine thank and you?. I'm very glad you are here. Because? you are a nice person. Yahoo"
 def Cleaning(x):#creating a function
-    x.lower()#lowe case
+    '''How does the function works? Basically I clean the data, I needed to lowercase, then splited by dots so it can separate sentences, this is important for the context.
+    Then I send it to another list in the loop (the loop reads each item of the list instead of the whole list) Z = 0 so it can start at the begining of the list and each time the loop ends it plus 1
+    so it goes to the next item on the list until the list its over, then I saved each word separately to the new list and that new list appended to the actual data set called all_Words.
+    '''
+    x = x.lower()#lowe case
     #newstring = x.split(".")#removing punctuation
     # x = ''.join(newstring)
-
+    #x = x.split(".")
+    #x = ''.join(letter for letter in x) 
+    #rint(x)
+    x = x.replace("?",".")
+    x = x.replace("!",".")
+    x = x.split(".")
+    print(x,"\n\n\n\n")
+    z = 0
+    new_list = []
+    for e in x:
+        e = x[z]
+        e = e.split(".")
+        new_list.append(e)
+        z = z+1
+    print(new_list)
+    z=0
+    for i in new_list:
+        i = x[z]
+        sent_list = []
+        res = ''.join(letter for letter in i if letter.isalnum() or letter.isspace())#removing special characters using isalnum() and exceptuating spaces
+        words = res.split()
+        sent_list.extend(words)
+        all_words.append(sent_list)
+        z = z+1
+    print(all_words)
+    #x = ''.join(letter for letter in x)
     newstring = ''.join(letter for letter in x if letter.isalnum() or letter.isspace())#removing special characters using isalnum() and exceptuating spaces
-    print(newstring)
-print(string,"\n")
+    #print(newstring)
+    
+#print(string,"\n")
 
 Cleaning(string)
-'''
+sent = ["Hello how are you?! I am trying to tell. you, you are beautifull!"]
 
 '''
 With NLTK
+'''
 '''
 def clean_text(text):
 
@@ -40,6 +73,7 @@ def clean_text(text):
     text = text.translate(str.maketrans('','',string.punctuation.replace(' ', '')))#remove punctuation without spaces
 
 #removing punctuation
+    #text = text.split(".")
     words = nltk.word_tokenize(text)#tokenizing or spliting the words
 
     #Lemmatize or stemming 
@@ -47,6 +81,7 @@ def clean_text(text):
     words = [lemmatizer.lemmatize(word) for word in words]
     print(words)
     return ''.join(words)
+'''
     
 
 def remove_numbers(text):
@@ -58,5 +93,5 @@ def remove_numbers(text):
 # print(newcontent)
 # no_numbers = remove_numbers(content)
 # print(no_numbers)
-data = clean_text(content)
+#data = clean_text(content)
 # print(data)
